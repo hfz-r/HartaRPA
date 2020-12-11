8e56d32b49e7b4cf5cd27871a331c97d7d7719d2
hfz-r <hafizlism@gmail.com>
2020-12-11 23:49:02 +0800

    update global settings

--- src/Services/File/File.API/FileSettings.cs
+++ src/Services/File/File.API/FileSettings.cs
@@ -3,5 +3,12 @@
     public class FileSettings
     {
         public string ConnectionString { get; set; }
+        public SourceFile SourceFile { get; set; }
+    }
+
+    public class SourceFile
+    {
+        public string Folder { get; set; }
+        public string[] Headers { get; set; }
     }
 }
